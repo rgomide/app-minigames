6886148721cbb675c0bd559ef9091d9ad90e441c
rgomide <renato.s.gomide@gmail.com>
2024-08-28 14:23:51 -0300

    atualizar esquema de navegação

--- App.js
+++ App.js
@@ -3,11 +3,26 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack'
 import { NavigationContainer } from '@react-navigation/native'
 
 import MainMenuScreen from './src/screens/MainMenuScreen'
+
+import AnagramaStartScreen from './src/screens/anagrama/AnagramaStartScreen'
+
+import AssociacaoStartScreen from './src/screens/associacao/AssociacaoStartScreen'
+
+import ForcaStartScreen from './src/screens/forca/ForcaStartScreen'
+
 import QuizStartScreen from './src/screens/quiz/QuizStartScreen'
 import QuizGameScreen from './src/screens/quiz/QuizGameScreen'
 import QuizResultScreen from './src/screens/quiz/QuizResultScreen'
 
-import { MAIN_MENU_SCREEN, QUIZ_START_SCREEN, QUIZ_GAME_SCREEN, QUIZ_RESULT_SCREEN } from './src/constants/screens'
+import {
+  MAIN_MENU_SCREEN,
+  QUIZ_START_SCREEN,
+  QUIZ_GAME_SCREEN,
+  QUIZ_RESULT_SCREEN,
+  ANAGRAMA_START_SCREEN,
+  ASSOCIACAO_START_SCREEN,
+  FORCA_START_SCREEN
+} from './src/constants/screens'
 
 const Stack = createNativeStackNavigator()
 
@@ -16,6 +31,27 @@ export default function App() {
     <NavigationContainer>
       <Stack.Navigator initialRouteName={MAIN_MENU_SCREEN} screenOptions={{ contentStyle: { backgroundColor: "#eee" } }}>
         <Stack.Screen name={MAIN_MENU_SCREEN} component={MainMenuScreen} options={{ title: 'Menu' }} />
+        <Stack.Group>
+          <Stack.Screen
+            name={ANAGRAMA_START_SCREEN}
+            component={AnagramaStartScreen}
+            options={{ title: 'Anagrama' }}
+          />
+        </Stack.Group>
+        <Stack.Group>
+          <Stack.Screen
+            name={ASSOCIACAO_START_SCREEN}
+            component={AssociacaoStartScreen}
+            options={{ title: 'Associação' }}
+          />
+        </Stack.Group>
+        <Stack.Group>
+          <Stack.Screen
+            name={FORCA_START_SCREEN}
+            component={ForcaStartScreen}
+            options={{ title: 'Forca' }}
+          />
+        </Stack.Group>
         <Stack.Group>
           <Stack.Screen
             name={QUIZ_START_SCREEN}
